ad3a2bd5ea6337ff09c253256abc623ad8484344
up202306990 <up202306990@edu.fe.up.pt>
2025-10-25 15:05:11 +0100

    Finished, it's working

--- src/application_layer.c
+++ src/application_layer.c
@@ -5,6 +5,108 @@
 
 #include <stdio.h>
 
+unsigned char* encodeControlPacket(const unsigned int c, const char* filename, long int filesize, unsigned int* packetsize) {
+
+    unsigned int tmp = filesize;
+    int L1 = 0;
+    do {
+        L1++;
+        tmp >>= 8;
+    } while (tmp > 0);
+    const int L2 = strlen(filename);
+
+    *packetsize = 1 + 2 + L1 + 2 + L2;
+    unsigned char *packet = (unsigned char*)malloc(*packetsize);
+    if (!packet) return NULL;
+
+    unsigned int pos = 0;
+    packet[pos++] = c;
+    packet[pos++] = 0;
+    packet[pos++] = L1;
+
+    for(int i = L1 - 1; i >= 0; i--) {
+        packet[pos++] = (filesize >> (8 * i)) & 0xFF;
+    }
+
+    packet[pos++] = 1;
+    packet[pos++] = L2;
+    memcpy(packet + pos, filename, L2);
+
+    return packet;
+}
+
+unsigned char* decodeControlPacket(const unsigned char* packet, unsigned int packetsize, unsigned long int* filesize) {
+    if(!packet || packetsize < 5 || !filesize) return NULL;
+
+    unsigned int pos = 0;
+    pos++;
+    if(packet[pos++] != 0) return NULL;
+
+    unsigned char L1 = packet[pos++];
+    if(L1 == 0 || pos + L1 > packetsize) return NULL;
+
+    *filesize = 0;
+    for(unsigned int i = 0; i < L1; i++) {
+        *filesize = (*filesize << 8) | packet[pos++];
+    }
+
+    if(packet[pos++] != 1) return NULL;
+
+    unsigned char L2 = packet[pos++];
+    if(L2 == 0 || pos + L2 > packetsize) return NULL;
+
+    unsigned char* filename = (unsigned char*)malloc(L2+1);
+    if(!filename) return NULL;
+    
+    memcpy(filename, packet + pos, L2);
+    filename[L2] = '\0';
+
+    return filename;
+}
+
+
+unsigned char* encodeDataPacket(const unsigned char* data, unsigned int datasize, unsigned int* packetsize) {
+    if(!data || datasize == 0 || !packetsize) return NULL;
+
+    unsigned char L1 = datasize % 256;
+    unsigned char L2 = datasize / 256;
+
+    *packetsize = 3 + datasize;
+    unsigned char* packet = (unsigned char*)malloc(*packetsize);
+    if(!packet) return NULL;
+
+    unsigned int pos = 0;
+
+    packet[pos++] = 2;
+    packet[pos++] = L2;
+    packet[pos++] = L1;
+
+    memcpy(packet + pos, data, datasize);
+    return packet;
+}
+
+unsigned char* decodeDataPacket(const unsigned char* packet, const unsigned int packetsize, unsigned int* datasize) {
+    if(!packet || packetsize < 3 || !datasize) return NULL;
+
+    unsigned int pos = 0;
+    
+    if(packet[pos++] != 2) return NULL;
+
+    unsigned char L2 = packet[pos++];
+    unsigned char L1 = packet[pos++];
+
+    *datasize = (256 * L2) + L1;
+
+    if(*datasize == 0 || (pos + *datasize) > packetsize) return NULL;
+
+    unsigned char* data = (unsigned char*)malloc(*datasize);
+    if(!data) return NULL;
+
+    memcpy(data, packet + pos, *datasize);
+
+    return data;
+}
+
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
@@ -127,7 +229,7 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
 
         printf("Receiving file: %s (%lu bytes)\n", receivedFilename, fileSize);
 
-        FILE *file = fopen((char*) receivedFilename, "wb"); // use argument filename as destination
+        FILE *file = fopen((char*) filename, "wb");
         if (!file) {
             perror("Error creating output file");
             free(receivedFilename);
@@ -190,7 +292,6 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
         fclose(file);
         free(receivedFilename);
 
-        // 3️⃣ --- Close link ---
         if (llclose() == -1) {
             perror("Failed to close link layer connection");
             exit(1);
@@ -198,106 +299,4 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
             printf("llclose() successful!\n");
         }
     }
-}
-
-unsigned char* encodeControlPacket(const unsigned int c, const char* filename, long int filesize, unsigned int* packetsize) {
-
-    unsigned int tmp = filesize;
-    int L1 = 0;
-    do {
-        L1++;
-        tmp >>= 8;
-    } while (tmp > 0);
-    const int L2 = strlen(filename);
-
-    *packetsize = 1 + 2 + L1 + 2 + L2;
-    unsigned char *packet = (unsigned char*)malloc(*packetsize);
-    if (!packet) return NULL;
-
-    unsigned int pos = 0;
-    packet[pos++] = c;
-    packet[pos++] = 0;
-    packet[pos++] = L1;
-
-    for(int i = L1 - 1; i >= 0; i--) {
-        packet[pos++] = (filesize >> (8 * i)) & 0xFF;
-    }
-
-    packet[pos++] = 1;
-    packet[pos++] = L2;
-    memcpy(packet + pos, filename, L2);
-
-    return packet;
-}
-
-unsigned char* decodeControlPacket(const unsigned char* packet, unsigned int packetsize, unsigned long int* filesize) {
-    if(!packet || packetsize < 5 || !filesize) return NULL;
-
-    unsigned int pos = 0;
-    pos++;
-    if(packet[pos++] != 0) return NULL;
-
-    unsigned char L1 = packet[pos++];
-    if(L1 == 0 || pos + L1 > packetsize) return NULL;
-
-    *filesize = 0;
-    for(unsigned int i = 0; i < L1; i++) {
-        *filesize = (*filesize << 8) | packet[pos++];
-    }
-
-    if(packet[pos++] != 1) return NULL;
-
-    unsigned char L2 = packet[pos++];
-    if(L2 == 0 || pos + L2 > packetsize) return NULL;
-
-    unsigned char* filename = (unsigned char*)malloc(L2+1);
-    if(!filename) return NULL;
-    
-    memcpy(filename, packet + pos, L2);
-    filename[L2] = '\0';
-
-    return filename;
-}
-
-
-unsigned char* encodeDataPacket(const unsigned char* data, unsigned int datasize, unsigned int* packetsize) {
-    if(!data || datasize == 0 || !packetsize) return NULL;
-
-    unsigned char L1 = datasize % 256;
-    unsigned char L2 = datasize / 256;
-
-    *packetsize = 3 + datasize;
-    unsigned char* packet = (unsigned char*)malloc(*packetsize);
-    if(!packet) return NULL;
-
-    unsigned int pos = 0;
-
-    packet[pos++] = 2;
-    packet[pos++] = L2;
-    packet[pos++] = L1;
-
-    memcpy(packet + pos, data, datasize);
-    return packet;
-}
-
-unsigned char* decodeDataPacket(const unsigned char* packet, const unsigned int packetsize, unsigned int* datasize) {
-    if(!packet || packetsize < 3 || !datasize) return NULL;
-
-    unsigned int pos = 0;
-    
-    if(packet[pos++] != 2) return NULL;
-
-    unsigned char L2 = packet[pos++];
-    unsigned char L1 = packet[pos++];
-
-    *datasize = (256 * L2) + L1;
-
-    if(*datasize == 0 || (pos + datasize) > packetsize) return NULL;
-
-    unsigned char* data = (unsigned char*)malloc(*datasize);
-    if(!data) return NULL;
-
-    memcpy(data, packet + pos, *datasize);
-
-    return data;
 }
